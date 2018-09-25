cdf5a11830066c5d9aa07f61dd9fcbdf4f72a973
ir013043zb <ir013043zb@H013043.NET>
2018-09-25 13:50:18 +0200

    add this files

---
+++ src/Example.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ir013043zb
+ *
+ */
+public class Example {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is an example");
+	}
+
+}
